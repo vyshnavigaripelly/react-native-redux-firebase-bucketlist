befaff165ddb92c030bbd4f7bcb4fcea30dbbedf
vyshnavigaripelly <41191430+vyshnavigaripelly@users.noreply.github.com>
2020-03-20 17:30:46 -0500

    Update and rename android/app/src/main/java/com/todo/MainActivity.java to android/app/src/main/java/com/bucketlist/MainActivity.java

--- android/app/src/main/java/com/bucketlist/MainActivity.java
+++ android/app/src/main/java/com/bucketlist/MainActivity.java
@@ -1,4 +1,4 @@
-package com.dwicao.mytodo;
+package com.dwicao.mybucketlist;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "todo";
+        return "bucketlist";
     }
 }
